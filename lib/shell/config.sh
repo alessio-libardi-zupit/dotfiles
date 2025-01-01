803d37f4bb8db8e166e0912eb40736c961673441
Alessio Libardi <115072949+alessio-libardi-zupit@users.noreply.github.com>
2025-01-01 09:51:39 +0000

    fix: change bash

--- lib/shell/config.sh
+++ lib/shell/config.sh
@@ -4,9 +4,9 @@ echo "Configuring libraries..."
 brew bundle --file=./lib/shell/Brewfile
 
 if [ "$(ps -p $$ -o 'comm=')" != "bash" ]; then
+  SHELL_PATH="/opt/homebrew/bin/bash"
   echo "Changing default shell to $SHELL_PATH"
 
-  SHELL_PATH="/opt/homebrew/bin/bash"
   grep -qxF "$SHELL_PATH" /etc/shells || echo "$SHELL_PATH" | sudo tee -a /etc/shells
   sudo chsh -s "$SHELL_PATH"
   exec "$SHELL_PATH"
